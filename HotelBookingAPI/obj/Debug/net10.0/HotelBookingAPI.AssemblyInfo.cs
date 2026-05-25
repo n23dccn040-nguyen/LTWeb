318ccd0ec0eb8450694a8d0e90fff24232ed7aca
n23dccn040-nguyen <n23dccn040@gmail.ptithcm.edu.vn>
2026-05-25 10:59:54 +0700

    thêm frontend

--- HotelBookingAPI/obj/Debug/net10.0/HotelBookingAPI.AssemblyInfo.cs
+++ HotelBookingAPI/obj/Debug/net10.0/HotelBookingAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelBookingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fba560f63b636e84f330e16da2f07428cc6ec64")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelBookingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelBookingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
